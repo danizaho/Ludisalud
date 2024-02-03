6066eafcfb3b4e6f7575fbbc93bccade7e39fc56
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2024-02-03 15:54:47 +0000

    primera version de introduccion lista

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,20 +7,20 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
+		<div className="inicio">
+		<h1>Bienvenid@s a Boardgames4All</h1>
+		<p>El juego es algo innato en el ser humano. Desde que nacemos la manera en la que nos aprendemos a relacionar
+			es a través de él. Y cuando mejor aprendemos, es de esta manera.</p>
+		<p>Y hasta hace muy poco,parecía que, al crecer, dejábamos de hacerlo, trabajando y buscando hobbys
+			que nos hicieran "crecer".</p>
+			<h5>Por suerte esto cambió</h5>	
+			<p>Y es que desde hace unos años y sobretodo con la pandemia, el mundo de los juegos de mesa ha 
+			experimentado un crecimiento brutal por parte de gente de todas las edades, siendo la pregunta más
+			clásica</p>
+			<h3>¿Qué juego me recomiendas</h3>
+			<p>Pues quédate aquí y lo sabrás</p>
+
+
 		</div>
 	);
 };
