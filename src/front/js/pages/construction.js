fcb65832732485d88ac31d10772d5832b07e62f8
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2024-03-27 21:39:02 +0000

    empezando a trabajar con pagina en construccion

--- src/front/js/pages/construction.js
+++ src/front/js/pages/construction.js
@@ -6,6 +6,13 @@ export const Construction = () =>{
     return(
         <div className="construccion">
             <h2>La página que esta buscando actualmente esta en construccion</h2>
+            <div className="post-it">
+                <p>Nuevo contenido</p>
+            </div>
+            <div className="post-it">
+                <p>Nuevas actividades</p>
+            </div>
+
         </div>
     )
 }
